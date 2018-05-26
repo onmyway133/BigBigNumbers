0be05ae9c10840d8b5e98ff6134e33a0cf7b2cfd
Khoa Pham <onmyway133@gmail.com>
2018-05-26 12:27:28 +0200

    Embed child view controller

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -7,19 +7,19 @@
 //
 
 import UIKit
+import Anchors
 
 class ViewController: UIViewController {
 
+  private let cameraController = CameraController()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+    add(childController: cameraController)
+    activate(
+      cameraController.view.anchor.edges
+    )
   }
 }
 
