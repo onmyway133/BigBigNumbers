56f046b3261a52eaa31e8f85345e100ddff153b4
Khoa Pham <onmyway133@gmail.com>
2018-05-26 18:01:53 +0200

    Run on viewDidAppear

--- Sources/ViewController.swift
+++ Sources/ViewController.swift
@@ -33,8 +33,8 @@ class ViewController: UIViewController {
     ocrService.delegate = self
   }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    super.touchesBegan(touches, with: event)
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
 
     musicService.play(fileName: "introduction")
   }
